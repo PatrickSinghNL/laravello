d87b776da0831f33ab8f85d803bf9279955d8efc
Patrick Singh <patrick.singh@bettyblocks.com>
2022-01-07 15:57:54 +0100

    Configured webpack mix to accept gql files. And added new directory for graphql queries

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,24 @@ const mix = require('laravel-mix');
     require("tailwindcss")
 ]);
 
-mix.js('resources/js/app.js', 'public/js')
-    .vue();
+mix.extend(
+    'graphql',
+    new class {
+
+        dependencies() {
+            return ['graphql', 'graphql-tag']
+        }
+
+        webpackRules() {
+            return {
+                test: /\.(graphql|gql)$/,
+                exclude: /node_modules/,
+                loader: 'graphql-tag/loader'
+            }
+        }
+
+    }()
+);
+
+mix.js('resources/js/app.js', 'public/js').vue();
+mix.graphql();
